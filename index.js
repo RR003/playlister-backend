6ef02ff64d121f385c8459fd25b916427040c997
RR003 <mail2rahulraja@gmail.com>
2022-12-15 22:46:59 -0500

    changes 4

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://video-playlister.web.app", "http://localhost:3000/"],
+    origin: ["https://video-playlister.web.app", "http://localhost:3000"],
     credentials: true,
   })
 );
