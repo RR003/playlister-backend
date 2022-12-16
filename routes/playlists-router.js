29d48e5d51ea7484dca47f88967986a448c1ce72
RR003 <mail2rahulraja@gmail.com>
2022-12-15 22:33:18 -0500

    changes 3

--- routes/playlists-router.js
+++ routes/playlists-router.js
@@ -9,15 +9,15 @@ const PlaylistController = require("../controllers/playlist-controller");
 const router = express.Router();
 const auth = require("../auth");
 
-router.post("/playlist", auth.verify, PlaylistController.createPlaylist);
-router.delete("/playlist/:id", auth.verify, PlaylistController.deletePlaylist);
-router.get("/playlist/:id", auth.verify, PlaylistController.getPlaylistById);
+router.post("/playlist", PlaylistController.createPlaylist);
+router.delete("/playlist/:id", PlaylistController.deletePlaylist);
+router.get("/playlist/:id", PlaylistController.getPlaylistById);
 router.get(
   "/playlistpairs/:q",
-  auth.verify,
+
   PlaylistController.getPlaylistPairs
 );
 router.get("/playlists/:q", PlaylistController.getPlaylists);
-router.put("/playlist/:id", auth.verify, PlaylistController.updatePlaylist);
+router.put("/playlist/:id", PlaylistController.updatePlaylist);
 
 module.exports = router;
